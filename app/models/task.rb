d2f934bfb652cf5f019926186cc65eb197c78fe0
Sowmya Gaja Kumar <sgkumar@west-10-232-109-35.tamulink.tamu.edu>
2018-11-13 15:38:09 -0600

    made changes to tasks to make title unique

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ApplicationRecord
 	belongs_to :user
-	validates :title, presence: true, on: :create
+    validates :title, :uniqueness => true, presence: true, on: :create
 	validates :target_value, presence: true, on: :create, numericality: {greater_than: 0}
 	validates :target_date, numericality: {greater_than_or_equal_to: Time.now.to_i, message: "must be after today"}
-
 end
