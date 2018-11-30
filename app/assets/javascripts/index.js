8c8c8e71b0303a34e47524707e83d04f67fa15b5
amurakonda <amurakonda@tamu.edu>
2018-12-01 01:07:39 +0530

    resolved bug 7

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,41 +9,39 @@ var ready = function(){
       $('#task_custom_measure').parent().hide("slow");
     }
   });
-  var count=0;
-  $("#admin_add").click(function(){
-    if (count!=0) {return;}
-    $.post(
-      "/admin/update",
-      {"email": $("#email_admin").val(), "func":"add"},
-      function(data,status) {
-        count=0;
-        if (status == "success"){
-          alert("successfully Added Admin");
-          location.reload();
-        } else {
-          alert(data);
-        }
+};
+var admin_add = function(){
+  if (count!=0) {return;}
+  $.post(
+    "/admin/update",
+    {"email": $("#email_admin").val(), "func":"add"},
+    function(data,status) {
+      count=0;
+      if (status == "success"){
+        alert("successfully Added Admin");
+        location.reload();
+      } else {
+        alert(data);
       }
-    );
-    count=1;
-    });
-    $("#admin_Remove").click(function(){
-      if(count!=0) {return;}
-      $.post(
-        "/admin/update",
-        {"email": $("#email_admin").val(), "func":"remove"},
-        function(data,status) {
-          count=0;
-          if (status == "success"){
-            alert("successfully Removed Admin");
-            location.reload();
-          } else {
-            alert(data);
-          }
-        }
-      );
-      count=1;
-    });
+    }
+  );
+};
+var admin_remove = function(){
+  if(count!=0) {return;}
+  $.post(
+    "/admin/update",
+    {"email": $("#email_admin").val(), "func":"remove"},
+    function(data,status) {
+      count=0;
+      if (status == "success"){
+        alert("successfully Removed Admin");
+        location.reload();
+      } else {
+        alert(data);
+      }
+    }
+  );
+  count=1;
 };
 $(document).ready(ready)
 $(document).on('turbolinks:load',ready)
